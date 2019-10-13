f5fccf1eeb2932c0dfde0b36bb9e96e4544f6f54
David Brown <dmlb2000@gmail.com>
2019-10-12 19:57:22 -0700

    minor fixes
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- tests/ingest_db_setup_test.py
+++ tests/ingest_db_setup_test.py
@@ -3,7 +3,7 @@
 """Test cart database setup class."""
 from unittest import TestCase
 from peewee import SqliteDatabase
-from pacifica.ingest.orm import IngestState
+from pacifica.ingest.orm import IngestState, IngestStateSystem
 
 
 class IngestDBSetup(TestCase):
@@ -13,14 +13,14 @@ class IngestDBSetup(TestCase):
     def setUp(self):
         """Setup the database with in memory sqlite."""
         self._db = SqliteDatabase('file:cachedb?mode=memory&cache=shared')
-        for model in [IngestState]:
+        for model in [IngestState, IngestStateSystem]:
             model.bind(self._db, bind_refs=False, bind_backrefs=False)
         self._db.connect()
-        self._db.create_tables([IngestState])
+        self._db.create_tables([IngestState, IngestStateSystem])
 
     def tearDown(self):
         """Tear down the database."""
-        self._db.drop_tables([IngestState])
+        self._db.drop_tables([IngestState, IngestStateSystem])
         self._db.close()
         self._db = None
     # pylint: enable=invalid-name
